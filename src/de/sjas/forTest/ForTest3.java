aa6c8c56c19b7b0bb00a5ba78afea6183fc2e99a
sjas <teilzeittresor@web.de>
2013-07-27 23:26:25 +0200

    added another for test

---
+++ src/de/sjas/forTest/ForTest3.java
@@ -0,0 +1,27 @@
+package de.sjas.forTest;
+
+/**
+ * To check if foreach works with simple arrays, and emtpy ones especially.
+ * 
+ * @author sjas
+ */
+public class ForTest3 {
+
+	public static void main(String[] args) {
+
+		int arrayLength = 5;
+		String[] testArray = new String[arrayLength];
+
+		for (String i : testArray) {
+			System.out
+					.println("Can java iterate over an emtpy array with foreach?");
+			System.out.println(i);
+		}
+
+		System.out.println("\nIt seems it can.");
+		System.out.println("And it does not care about null's.");
+
+		System.out.println("\nDONE!");
+	}
+
+}
