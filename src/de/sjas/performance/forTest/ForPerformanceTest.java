baa9740936c8d217f578c27fd6e1c57e55c588d5
sjas <teilzeittresor@web.de>
2013-10-15 14:48:35 +0200

    minor changes, commenting

--- src/de/sjas/performance/forTest/ForPerformanceTest.java
+++ src/de/sjas/performance/forTest/ForPerformanceTest.java
@@ -4,16 +4,15 @@
 package de.sjas.performance.forTest;
 
 /**
- * Test performance difference of using array.length vs. predefined variable. To
- * get the most accurate results, the for-loops' bodies were left empty at
- * first. This did not work out, so the console prints were added.
+ * Testing performance of for loops.
  * 
- * Bottom line is, the optimization does not really make a big difference in
- * java. Usually it's like ~5% faster, but sometimes the optimized version is
- * even slower than the regular one.
+ * 1. regular
  * 
- * Well, this could differ in other languages, as things are implementation
- * dependent.
+ * 2. getting length during initialization phase, so no object access is needed
+ * 
+ * 3. getting length prior to for loop
+ * 
+ * RESULT = no differences.
  * 
  * Instead of using System.currentTimeMillis(), there is also System.nanoTime()
  * available. First is the absolute time via the system's clock, the latter is
@@ -33,31 +32,34 @@ public class ForPerformanceTest {
 		long testStartInMs;
 		long testEndInMs;
 
-		// the actual test runs
-
-		// First test: Usual case
+		// first test: usual case
 		testStartInMs = System.currentTimeMillis();
 		for (int i = 0; i < testArray.length; i++) {
 			System.out.print("");
 		}
 		testEndInMs = System.currentTimeMillis();
-
-		// First Test Results
 		System.out.print("First Test Duration:\t");
 		System.out.println("" + (testEndInMs - testStartInMs));
 
-		// Second test: read array length in initialization phase of loop
+		// second test: read array length in initialization phase of loop
 		testStartInMs = System.currentTimeMillis();
 		for (int i = 0, arrayLength = testArray.length; i < arrayLength; i++) {
 			System.out.print("");
 		}
 		testEndInMs = System.currentTimeMillis();
-
-		// Second Test Results
 		System.out.print("Second Test Duration:\t");
 		System.out.println("" + (testEndInMs - testStartInMs));
-		System.out.println();
 
-		System.out.println("DONE!");
+		// third test
+		testStartInMs = System.currentTimeMillis();
+		int arrayLength = testArray.length;
+		for (int i = 0; i < arrayLength; i++) {
+			System.out.print("");
+		}
+		testEndInMs = System.currentTimeMillis();
+		System.out.print("Third Test Duration:\t");
+		System.out.println("" + (testEndInMs - testStartInMs));
+
+		System.out.println("\n\nDONE!");
 	}
 }
